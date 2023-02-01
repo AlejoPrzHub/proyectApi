602f666e77bcbbc68d99de6bfd4746e7e058b648
AlejoPrzHub <josealeprz@gmail.com>
2023-02-01 04:19:41 +0100

    "commit"

--- proyectApi/src/routes/app.js
+++ proyectApi/src/routes/app.js
@@ -1,11 +1,11 @@
 const express= require("express")
 const cors = require("cors")
 const errorHandling = require ("../error/errorHandling")
-const usuarioRoutes = require("../routes/usuario.routes")
-const librosRoutes = require("../routes/libros.routes")
+
+
 const app = express();
 
-app.set("port", process.env.PORT|| 3000)
+//app.set("port", process.env.PORT|| 3000)
 app.use(cors());
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
@@ -17,7 +17,7 @@ app.use (function(req, res, next)
                         ({
                             error:true,
                             codigo: 404,
-                            message:"Upss, algo no va bien"
+                            message:"Upss, something went wrong!"
                         })
         })
 app.use(errorHandling);
